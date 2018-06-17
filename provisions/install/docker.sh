0a45e540e27d3da4470781ffe5c00d77f59112db
Cory Sabol <cory@secureideas.com>
2018-06-17 00:51:59 -0400

    added if to check if docker group already exists, add root and ahab to docker group

--- provisions/install/docker.sh
+++ provisions/install/docker.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 docker_url=https://download.docker.com/linux/ubuntu/dists/trusty/pool/stable/amd64/docker-ce_18.03.1~ce-0~ubuntu_amd64.deb 
 
@@ -12,6 +12,12 @@ echo "installing docker..."
 curl $docker_url 2>/dev/null > docker.deb
 dpkg -i docker.deb > /dev/null 2>&1
 apt-get -f --yes --force-yes install
-groupadd docker
+if [[ ! $(grep -E "^docker" /etc/group) ]]; then
+	echo "docker group not found, adding..."
+	groupadd docker
+fi
+echo "adding $USER to docker group..."
 usermod -aG docker $USER
+echo "adding ahab to docker group..."
+usermod -aG docker ahab
 # TODO: configure docker socket to accept remote connections as well.
