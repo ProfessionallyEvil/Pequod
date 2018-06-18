36c23e0f954a7e1d8e7791c8c61953d79f43e367
Cory Sabol <cory@secureideas.com>
2018-06-18 09:31:59 -0400

    removed run.sh in place of start_docker_targets.sh script, to start up all docker targets.

--- provisions/targets/docker_socket/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pushd /vagrant/provisions/targets/docker_socket/
-echo "Setting up docker socket target..."
-docker build . -t docker_sock_mount
-docker run -v /var/run/docker.sock:/var/run/docker.sock:ro --name mody_dick docker_sock_mount &
---
+++ provisions/targets/start_docker_targets.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+pushd /home/ahab/.scripts/provisions/targets/docker_socket/
+echo "Setting up docker socket target..."
+docker build . -t docker_sock_mount
+popd
+echo "Starting up docker target: docker_sock_mount..."
+docker run -d -v /var/run/docker.sock:/var/run/docker.sock:ro --name this_better_work docker_sock_mount
+echo "BLAH..."
